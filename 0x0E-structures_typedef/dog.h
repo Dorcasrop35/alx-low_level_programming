aca4b511ebad9420b15f2aaeee927337d83fc397
Dorcasrop35 <dorcaschelangat26@gmail.com>
2023-09-11 12:13:29 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,22 +1,17 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
-// Define a structure named 'Person' to represent a person's information
-struct Person {
-    char name[50];  // Name of the person
-    int age;        // Age of the person
+/**
+ * struct Dog - A structure to represent information about a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
 };
 
-int main(void) {
-    // Declare and initialize a variable of type 'struct Person'
-    struct Person person1;
-    
-    // Fill in the information for person1
-    strcpy(person1.name, "John");
-    person1.age = 30;
-    
-    // Print the information
-    printf("Person's Name: %s\n", person1.name);
-    printf("Person's Age: %d\n", person1.age);
-    
-    return 0;
-}
+#endif 
