f51041ba126d8414a912c4cdf4bfd4cc077981d5
Dorcasrop35 <dorcaschelangat26@gmail.com>
2023-09-13 05:35:24 -0700

    0-print_name.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,10 +3,8 @@
 
 #include <stddef.h>
 
-/* Prototype for print_name function */
 void print_name(char *name, void (*f)(char *));
 
-/* Prototype for _putchar function */
 int _putchar(char c);
 
-#endif /* FUNCTION_POINTERS_H */
+#endif 
