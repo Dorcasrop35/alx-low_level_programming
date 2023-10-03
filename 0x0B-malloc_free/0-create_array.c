e87c64a7966c1be90edaefe143607e4050dd80bf
Dorcasrop35 <dorcaschelangat26@gmail.com>
2023-10-03 06:34:17 -0700

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *array;
+
+    if (size == 0)
+        return (NULL);
+
+    array = malloc(size * sizeof(char));
+
+    if (array == NULL)
+        return (NULL);
+
+    for (unsigned int i = 0; i < size; i++)
+        array[i] = c;
+
+    return (array);
+}
