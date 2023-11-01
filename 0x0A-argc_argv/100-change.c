b4fcdb8867f4b50465f047a4b79db74f12614832
Dorcasrop35 <dorcaschelangat26@gmail.com>
2023-11-01 00:38:17 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,43 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
- *
- * Return: 0 for success, 1 for error
- */
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
+int main(int argc, char *argv[]) {
+    int cents;
+    int i;
+    int min_coins = 0;
+    int coins[] = {25, 10, 5, 2, 1};
+    int num_coins = sizeof(coins) / sizeof(coins[0]);
 
-	int cents = atoi(argv[1]);
+    if (argc != 2) {
+        printf("Error\n");
+        return 1;
+    }
 
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (0);
-	}
+    cents = atoi(argv[1]);
 
-	int coins[] = {25, 10, 5, 2, 1};
-	int num_coins = sizeof(coins) / sizeof(coins[0]);
-	int num = 0;
+    if (cents < 0) {
+        printf("0\n");
+        return 0;
+    }
 
-	for (int i = 0; i < num_coins; i++)
-	{
-		while (cents >= coins[i])
-		{
-			cents -= coins[i];
-			num++;
-		}
-	}
+    for (i = 0; i < num_coins; i++) {
+        while (cents >= coins[i]) {
+            cents -= coins[i];
+            min_coins++;
+        }
+    }
 
-	printf("%d\n", num);
-	return (0);
+    printf("%d\n", min_coins);
+
+    return 0;
 }
-
