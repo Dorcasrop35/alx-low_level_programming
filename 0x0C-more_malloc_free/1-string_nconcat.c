e88331ba326ce5f4d6e4f7d57f4c4a0af93cdbf5
Dorcasrop35 <dorcaschelangat26@gmail.com>
2023-11-03 11:18:03 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,46 +1,45 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenates two strings with a limit.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The maximum number of characters to concatenate from s2.
- * Return: A newly allocated string containing the concatenation of s1 and the
- *         first n characters of s2, or NULL on failure.
+ * string_nconcat - Concatenates two strings with n bytes of s2
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The maximum number of bytes from s2 to concatenate
+ *
+ * Return: A pointer to the newly allocated concatenated string
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n
-{	
-	char *concat;
-	unsigned int len1 = 0, len2 = 0;
-	unsigned int i, j;
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int s1_len = 0, s2_len = 0, i;
+	char *concatenated;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
+	while (s1[s1_len] != '\0')
+		s1_len++;
 
-	if (n >= len2)
-		n = len2;
+	while (s2[s2_len] != '\0')
+		s2_len++;
 
-	concat = (char *)malloc(len1 + n + 1);
+	if (n >= s2_len)
+		n = s2_len;
 
-	if (concat == NULL)
+	concatenated = malloc(s1_len + n + 1);
+
+	if (concatenated == NULL)
 		return (NULL);
 
-	for (i = 0; i < len1; i++)
-		concat[i] = s1[i];
+	for (i = 0; i < s1_len; i++)
+		concatenated[i] = s1[i];
 
-	for (j = 0; j < n; j++)
-		concat[i + j] = s2[j];
+	for (i = 0; i < n; i++)
+		concatenated[s1_len + i] = s2[i];
 
-	concat[i + j] = '\0';
+	concatenated[s1_len + n] = '\0';
 
-	return (concat);
+	return (concatenated);
 }
